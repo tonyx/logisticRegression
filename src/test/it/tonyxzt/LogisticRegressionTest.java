cb3ba5276678d7dd624b33795142058d5e24b8ee
Antonio Lucca <tonyx@yahoo.com>
2013-07-15 22:25:46 +0100

    rename

--- src/test/it/tonyxzt/LogisticRegressionTest.java
+++ src/test/it/tonyxzt/LogisticRegressionTest.java
@@ -40,40 +40,40 @@ public class LogisticRegressionTest {
         // given
         int numberOfFeatures = 3;
         double[] theta;
-        int valueOfFeatureParameters[][] = new int[7][numberOfFeatures];
-        valueOfFeatureParameters[0] = new int[]{1,100,80};
-        valueOfFeatureParameters[1] = new int[]{1,60,70};
-        valueOfFeatureParameters[2] = new int[]{1,30,40};
-        valueOfFeatureParameters[3] = new int[]{1,15,20};
-        valueOfFeatureParameters[4] = new int[]{1,5,8};
-        valueOfFeatureParameters[5] = new int[]{1,5,90};
-        valueOfFeatureParameters[6] = new int[]{1,0,130};
+        int parameters[][] = new int[7][numberOfFeatures];
+        parameters[0] = new int[]{1,100,80};
+        parameters[1] = new int[]{1,60,70};
+        parameters[2] = new int[]{1,30,40};
+        parameters[3] = new int[]{1,15,20};
+        parameters[4] = new int[]{1,5,8};
+        parameters[5] = new int[]{1,5,90};
+        parameters[6] = new int[]{1,0,130};
 
         // and
-        int extendedFeatures[][] = new int[7][10];
+        int extendedParameters[][] = new int[7][10];
 
         // wrap to quadratic and cubic parameters to learn also non linearly separable classifiers
-        for (int i=0;i<valueOfFeatureParameters.length;i++) {
-            extendedFeatures[i][0] = valueOfFeatureParameters[i][0]; // =1
-            extendedFeatures[i][1] = valueOfFeatureParameters[i][1];
-            extendedFeatures[i][2] = valueOfFeatureParameters[i][2];
+        for (int i=0;i<parameters.length;i++) {
+            extendedParameters[i][0] = parameters[i][0]; // =1
+            extendedParameters[i][1] = parameters[i][1];
+            extendedParameters[i][2] = parameters[i][2];
 
-            extendedFeatures[i][3] = valueOfFeatureParameters[i][1]^2;
-            extendedFeatures[i][4] = valueOfFeatureParameters[i][2]^2;
-            extendedFeatures[i][5] = valueOfFeatureParameters[i][1]*valueOfFeatureParameters[i][2];
-            extendedFeatures[i][6] = valueOfFeatureParameters[i][1]^3;
-            extendedFeatures[i][7] = valueOfFeatureParameters[i][2]^3;
-            extendedFeatures[i][8] = valueOfFeatureParameters[i][1]^3*valueOfFeatureParameters[i][2]^2;
-            extendedFeatures[i][9] = valueOfFeatureParameters[i][2]^3*valueOfFeatureParameters[i][1]^2;
+            extendedParameters[i][3] = parameters[i][1]^2;
+            extendedParameters[i][4] = parameters[i][2]^2;
+            extendedParameters[i][5] = parameters[i][1]*parameters[i][2];
+            extendedParameters[i][6] = parameters[i][1]^3;
+            extendedParameters[i][7] = parameters[i][2]^3;
+            extendedParameters[i][8] = parameters[i][1]^3*parameters[i][2]^2;
+            extendedParameters[i][9] = parameters[i][2]^3*parameters[i][1]^2;
         }
         double featuresExpectedValues[] = new double[]{1.0,0.0,1.0,0.0,1.0,0.0,0.0};
 
         // when
-        theta = LogisticRegresion.logisticTrain( extendedFeatures, featuresExpectedValues);
+        theta = LogisticRegresion.logisticTrain( extendedParameters, featuresExpectedValues);
 
         // then
-        for(int k = 0;k<extendedFeatures.length;k++) {
-            Assert.assertEquals(featuresExpectedValues[k], LogisticRegresion.logisticFunction(LogisticRegresion.scalarProduct(theta,extendedFeatures[k])), 0.1);
+        for(int k = 0;k<extendedParameters.length;k++) {
+            Assert.assertEquals(featuresExpectedValues[k], LogisticRegresion.logisticFunction(LogisticRegresion.scalarProduct(theta,extendedParameters[k])), 0.1);
         }
     }
 }
